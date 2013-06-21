39428046af6369e7c8992a4ee267eeff78170973
Mischa Taylor <mischa@misheska.com>
2013-06-21 14:07:21 -0700

    Automatically update root certs, otherwise Omnibus Installer will fail on CentOS 5.4 & 5.7

--- definitions/.centos/base.sh
+++ definitions/.centos/base.sh
@@ -1,5 +1,8 @@
 # Base install
 
+# Update root certs (otherwise Chef client won't install)
+wget -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
+
 # Disable fastest mirrors plugin
 sed -i "s|enabled=1|enabled=0|" /etc/yum/pluginconf.d/fastestmirror.conf
 
